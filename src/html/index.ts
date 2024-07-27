ec6262ebed7e54deaa11a26bae99e424c1118f78
chaitanya-Uike <chaitanyaguike@gmail.com>
2024-07-27 11:36:36 +0530

    export html tagged function

--- src/html/index.ts
+++ src/html/index.ts
@@ -1,10 +1,7 @@
 import HTMLParser, { Element } from "./parser";
 import lexer from "./lexer";
 
-export default function html(
-  strings: TemplateStringsArray,
-  ...values: any[]
-): Element {
+export function html(strings: TemplateStringsArray, ...values: any[]): Element {
   const tokens = lexer(strings, values);
   const template = getTemplate(strings, values);
   const parser = new HTMLParser(tokens, template);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "./signals";
 export * from "./dom";
+export * from "./html";
 export { createRoot, unTrack } from "./core";
